112638d0966eccb0888886d73abf67c22b3cf88c
aepfel <dertypausfloeha@gmail.com>
2013-10-21 11:24:19 +0200

    frontend funktioniert, ist aber noch ohne struktur

--- frontend.php
+++ frontend.php
@@ -9,13 +9,37 @@
 	if (!$link) {
 	    die('Verbindung schlug fehl: ' . mysql_error());
 	}
-	echo 'Erfolgreich verbunden';
+	//echo 'Erfolgreich verbunden';
 	mysql_select_db('bm');
-	$sql = 'SELECT msgid FROM bm WHERE bmaddress=$bma';
-	$result = mysql_query ( $sql );
-	$row = mysql_fetch_row ( $result );
-	echo $row[1];
+	$sql = "SELECT msgid FROM bm WHERE bmaddress='".$bma."';";
+	//echo $sql;
+	$result = mysql_query($sql);
+	if($result === FALSE) {
+	    die(mysql_error()); // TODO: better error handling
+	}
+	while($row = mysql_fetch_array($result))
+	{
+	    $msgid=$row[0];
+	}
+	//$row = mysql_fetch_row($result);
+	//echo "row:".$row[1].'\n';
+	$command = '/usr/bin/python /home/ae/bitkram/subjectbyid.py '.$msgid;
+	$temp = exec($command, $subject);
+	echo "<h1>";
+	for ($i=0; ($i < count($subject)) && ($i < 3); $i++)
+		{
+		echo $subject[$i]."<br>";
+		}
+	echo "</h1>";
+	$command = '/usr/bin/python /home/ae/bitkram/msgbyid.py '.$msgid;
+	$temp = exec($command, $msg);
+	//echo $output[0];
+	for($i=0; $i < count($msg); $i++)
+   		{
+		echo $msg[$i]."<br>";
+   		}
 	mysql_close($link);
-	echo '<p>Hallo Welt adresse:$bma</p>'; ?>
+	//echo "<p>msgid:".$msgid."</p>";
+?>
  </body>
 </html>
---
+++ msgbyid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import xmlrpclib
+import json
+import time
+import requests
+import urllib
+import sys
+
+
+bmapi = xmlrpclib.ServerProxy("http://apiich:apiichpasswort@localhost:8442/")
+print json.loads(bmapi.getInboxMessageByID(sys.argv[1]))['inboxMessage'][0]['message'].decode('base64')
+
